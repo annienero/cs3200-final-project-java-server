32e68b1832da423f7bcd0ac8f8d6315172b87f92
Annie <annienero93@gmail.com>
2018-08-16 12:46:33 -0400

    json ignore for uploader

--- src/main/java/com/example/cs3200finalprojectjavaserver/models/Video.java
+++ src/main/java/com/example/cs3200finalprojectjavaserver/models/Video.java
@@ -1,5 +1,7 @@
 package com.example.cs3200finalprojectjavaserver.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -21,6 +23,7 @@ public class Video {
     private double avgCuteness;
     private double avgSadness;
     @ManyToOne()
+    @JsonIgnore
     @JoinColumn(name="uploader_id")
     private User uploader;
     @OneToMany()
